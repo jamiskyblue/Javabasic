faf3a95966b51683564e956b7c151a7e7a931fbc
MD JARIF BOKSH <skyblue@MDs-MacBook-Pro.local>
2019-08-06 22:10:53 -0400

    first commit

---
+++ src/base/Variables.java
@@ -0,0 +1,12 @@
+package base;
+
+public class Variables {
+	
+    	 //
+         static String name = "MD JAMIL BOKSH";
+    	 static String email = "jami@gmail.com";
+    	 static String student_id = "3973NY";
+    	 public static void main(String[]args) {
+    	 System.out.println(name+email+student_id);
+      }
+}
---
+++ src/base/Variables2.java
@@ -0,0 +1,6 @@
+package base;
+
+public class Variables2 {
+//Create An Object
+	//desired class name + resferrance Variable = new(keyword) + class Name()
+}
